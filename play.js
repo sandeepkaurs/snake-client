55d738112db706c5e0ac9b3e4ff3197d2f5855c7
sandeepkaurs <sandeep.singh@telus.com>
2022-11-24 19:40:52 -0800

    committing play.js after creating setupInput and handleUserInput

--- play.js
+++ play.js
@@ -2,3 +2,17 @@ const {connect} = require('./client');
 
 console.log("Connecting ...");
 const conn = connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", key => {
+    handleUserInput = (key); 
+  });
+  return stdin;
+};
