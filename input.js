68fb7738596afaaee13f5b4fe163b17fdd7cf8d2
sandeepkaurs <sandeep.singh@telus.com>
2022-11-24 20:35:01 -0800

    committing all changes made so far, up until WASD is completed

--- input.js
+++ input.js
@@ -1,12 +1,34 @@
-const setupInput = function (conn) {
+// Stores the active TCP connection object.
+let connection;
+
+const moveUp = "w";
+const moveDown = "s";
+const moveLeft = "a";
+const moveRight = "d";
+
+const handleUserInput = function(key) {
+  console.log("pressed", key)
+  if (key === moveUp) {
+    connection.write('Move: up');
+  }
+  if (key === moveDown) {
+    connection.write('Move: down');
+  }
+  if (key === moveLeft) {
+    connection.write('Move: left');
+  }
+  if (key === moveRight) {
+    connection.write('Move: right');
+  }
+}
+
+const setupInput = (conn) => {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", key => {
-    handleUserInput = (key); 
-  });
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
--- play.js
+++ play.js
@@ -2,8 +2,7 @@ const { connect } = require('./client');
 const { setupInput } = require('./input.js');
 
 console.log("Connecting ...");
-const conn = connect();
+// One way to fix this is to pass the conn object returned by connect() to the setUpInput function. In truth, this is not the most ideal solution, but it is simple and good enough for our purposes.
+setupInput(connect());
 
 // setup interface to handle user input from stdin
-
-
